f2ff527bd5377abfc246ccadbd33645474f1d2bd
Josue Perez <josueperezf@gmail.com>
2019-10-19 21:26:02 -0300

    le corregi base de datos

--- Crud-Mysql-php.sql
+++ Crud-Mysql-php.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE bodega;
 use bodega;
 CREATE TABLE bodegas (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `nombre` varchar(100) NOT NULL,
   `direccion` varchar(100) NOT NULL,
   `estatus` char(1) DEFAULT NULL
@@ -14,7 +14,4 @@ INSERT INTO `bodegas` (`id`, `nombre`, `direccion`, `estatus`) VALUES
 (3, 'ADMINISTRACION2', 'SAN CRISTOBAL', '1'),
 (4, 'DIRECCION', 'SAN CRISTOBAL', '1'),
 (5, 'CAJA DE AHORRO', 'SAN CRISTOBAL', '1'),
-(6, 'RELACIONES CORPORATIVAS', 'SAN CRISTOBAL', '1'),
-
-ALTER TABLE `bodegas`
-  ADD PRIMARY KEY (`id`);
+(6, 'RELACIONES CORPORATIVAS', 'SAN CRISTOBAL', '1');
